bddbbbb995a71aabecca89373fe1dc5495691808
hugo <hugomichaelsaavedra@gmail.com>
2017-03-26 15:19:21 -0700

    Add test-func for testing

--- ch1/list.rkt
+++ ch1/list.rkt
@@ -135,10 +135,46 @@
           (remove s (cdr los))
           (cons (car los) (remove s (cdr los)))))))
 
-(remove 'a '(a b c d e f g))   ;; Expected: '(b c d e f g)
-(remove 'a '(b c d e a f g h)) ;; Expected: '(b c d e f g h) 
-(remove 'a '(b d e f g h i))   ;; Expected: '(b d e f g h i)
-(remove 'a '(a b c d a b c d)) ;; Expected: '(b c d b c d)
-(remove 'a '(d b c d a b c d)) ;; Expected: '(d b c d b c d)
-(remove 'a '())                ;; Expected: '()
-(remove 'a '(d b c d b c d a)) ;; Expected: '(d b c d b c d)
+; (remove 'a '(a b c d e f g))   ;; Expected: '(b c d e f g)
+; (remove 'a '(b c d e a f g h)) ;; Expected: '(b c d e f g h) 
+; (remove 'a '(b d e f g h i))   ;; Expected: '(b d e f g h i)
+; (remove 'a '(a b c d a b c d)) ;; Expected: '(b c d b c d)
+; (remove 'a '(d b c d a b c d)) ;; Expected: '(d b c d b c d)
+; (remove 'a '())                ;; Expected: '()
+; (remove 'a '(d b c d b c d a)) ;; Expected: '(d b c d b c d)
+
+;; test-func : Func x List-of-FuncArgs x List-of-Expecteds -> List-of-Bool
+
+;; usage:  (test-func func func-tests expected) = lob, where lob is 
+;;         a list of booleans indicating whether the test 
+;;         in the corresponding position of func-tests passed or 
+;;         failed (i.e. matched the value in the corresponding position of
+;;         expected). for example, if all tests pass, all elements
+;;         of lob will be #t. likewise, if all fail, all will be
+;;         #f. If func-tests is null, then the empty list is returned.         
+
+(define test-func
+  (lambda (func func-tests expected)
+    (if (null? func-tests) '()
+;; TODO -- currently assume func returns no errors.
+;;         Refactor to return #f if func errors. 
+        (if (equal? (apply func (car func-tests)) (car expected))
+          (cons #t (test-func func (cdr func-tests) (cdr expected)))
+          (cons #f (test-func func (cdr func-tests) (cdr expected)))))))
+    
+(define remove-tests (list (list 'a '(a b c d e f g))    ;; Expected: '(b c d e f g)
+                           (list 'a '(b c d e a f g h))  ;; Expected: '(b c d e f g h) 
+                           (list 'a '(b d e f g h i))    ;; Expected: '(b d e f g h i)
+                           (list 'a '(a b c d a b c d))  ;; Expected: '(b c d b c d)
+                           (list 'a '(d b c d a b c d))  ;; Expected: '(d b c d b c d)
+                           (list 'a '())                 ;; Expected: '()
+                           (list 'a '(d b c d b c d a)))) ;; Expected: '(d b c d b c d)
+
+
+(define remove-expected (list (list 'b 'c 'd 'e 'f 'g)
+                              (list 'b 'c 'd 'e 'f 'g 'h)
+                              (list 'b 'd 'e 'f 'g 'h 'i)
+                              (list 'b 'c 'd 'b 'c 'd)
+                              (list 'd 'b 'c 'd 'b 'c 'd)
+                              (list)
+                              (list 'd 'b 'c 'd 'b 'c 'd)))
