082b78d9a36acc5b31a959422d586de2c6b5bf2a
hugo <hugomichaelsaavedra@gmail.com>
2017-03-28 05:58:40 -0700

    Add solutions to exs 1.16 and 1.18

--- ch1/exercises.rkt
+++ ch1/exercises.rkt
@@ -18,6 +18,26 @@
 ; (duple 1 '(blah))
 ; (duple 3 0)
 
+;; SchemeList-of-pairs ::= '() | ( Pair . SchemeList-of-Pairs ) 
+;;               Pair  ::= ( SchemeVal . SchemeVal ) 
+
+;; invert : SchemeList-of-pairs -> SchemeList-of-pairs
+;; usage: (invert lst) = a SchemeList-of-pairs, with the head
+;;                       and cdr of each pair interposed. 
+
+(define invert
+  (lambda (lst)
+    (if (null? lst) '()
+        (cons (list (car (cdr (car lst))) (car (car lst))) 
+              (invert (cdr lst))))))
+
+; tests
+; (invert '((1 a) (1 b) (1 c) (1 d)))
+; (invert '())
+; (invert '((1 2)))
+; (invert '((1 2) (3 4) (5 6)))
+
+
 ;; down : SchemeList -> SchemeList
 ;; usage: (down lst) = A list with elements of lst wrapped 
 ;;                     in parentheses. 
@@ -27,8 +47,26 @@
     (if (null? lst) '()
         (cons (list (car lst)) (down (cdr lst))))))
 
-
-(down '(1 2 3))
-(down '((a) (fine) (idea)))
-(down '(a (more (complicated)) object))
+; tests
+; (down '(1 2 3))
+; (down '((a) (fine) (idea)))
+; (down '(a (more (complicated)) object))
     
+;; swapper : SchemeVal x SchemeVal x SchemeList -> SchemeList
+;; usage: (swapper s1 s2 slist) = a new SchemeList with any occurrences
+;;                                of s2 are replaced with s1. 
+
+(define swapper
+  (lambda (s1 s2 slist)
+    (if (null? slist) '()
+        (if (equal? (car slist) s2) 
+            (cons s1 (swapper s1 s2 (cdr slist)))
+            (cons (car slist) (swapper s1 s2 (cdr slist)))))))
+
+; tests
+(swapper 'a 'd '(a b c d))
+(swapper 'a 'd '(a a a a d d d d ))
+(swapper 'a 'b '())
+(swapper '(1 2) '(a a) '((1 2) (1 2) (a a)))
+(swapper 'a 'a '(a b c d e a))
+(swapper '() 'a '(a b c d a a a))
